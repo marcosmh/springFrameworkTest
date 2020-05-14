955aaf0e707e033a0dc4f93f735580329036db9f
Marcos Magagaña Hernández <marcosmh@MacBook-Pro-de-Marcos.local>
2020-05-14 15:13:35 -0500

    conexion con el api rest

--- projectAngularSpring5/clientes-app/src/app/clientes/cliente.service.ts
+++ projectAngularSpring5/clientes-app/src/app/clientes/cliente.service.ts
@@ -3,14 +3,21 @@ import { Cliente } from './cliente';
 import { CLIENTES } from './clientes.json';
 import { Observable } from 'rxjs';
 import { of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class ClienteService {
 
-  constructor() { }
+  private urlEndPoint: string = 'http://localhost:8090/api/clientes';
+  constructor(private http: HttpClient) { }
 
   getClientes(): Observable<Cliente[]> {
-    return of(CLIENTES);
+    //return of(CLIENTES);
+    //return this.http.get<Cliente[]>(this.urlEndPoint);
+    return this.http.get(this.urlEndPoint).pipe(
+      map(response => response as Cliente[])
+    );
   }
 
 
