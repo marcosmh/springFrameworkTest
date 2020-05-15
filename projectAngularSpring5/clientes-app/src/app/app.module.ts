cfe11b1e18f57f1d655238652cd14cf678b322c5
Marcos Magagaña Hernández <marcosmh@MacBook-Pro-de-Marcos.local>
2020-05-14 19:50:51 -0500

    form clientes

--- projectAngularSpring5/clientes-app/src/app/app.module.ts
+++ projectAngularSpring5/clientes-app/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import {RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
 import { HeaderComponent} from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { FormComponent } from './clientes/form.component';
 import { DirectivaComponent } from './directiva/directiva.component';
 import { ClientesComponent } from './clientes/clientes.component';
 import { ClienteService } from './clientes/cliente.service';
-import {RouterModule, Routes } from '@angular/router';
+
 
 const routes: Routes = [
   { path: '', redirectTo: '/clientes', pathMatch: 'full'},
   { path: 'directivas', component: DirectivaComponent},
   { path: 'clientes', component: ClientesComponent},
+  { path: 'clientes/form', component: FormComponent},
 
 ];
 
@@ -24,11 +28,13 @@ const routes: Routes = [
     HeaderComponent,
     FooterComponent,
     DirectivaComponent,
-    ClientesComponent
+    ClientesComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
     RouterModule.forRoot(routes)
   ],
   providers: [ClienteService],
