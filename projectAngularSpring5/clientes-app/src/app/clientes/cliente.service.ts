72cd2a501c973f766167289d6fc54f55417dcba7
Marcos Magagaña Hernández <marcosmh@MacBook-Pro-de-Marcos.local>
2020-05-18 12:27:14 -0500

    Implementacion del metodo crear en el cliente.service y cliente.form

--- projectAngularSpring5/clientes-app/src/app/clientes/cliente.service.ts
+++ projectAngularSpring5/clientes-app/src/app/clientes/cliente.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { Cliente } from './cliente';
 import { Observable } from 'rxjs';
 import { of } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
 @Injectable()
 export class ClienteService {
 
+  private httpHeaders = new HttpHeaders({'Content-type': 'application/json'});
   private urlEndPoint: string = 'http://localhost:8090/api/clientes';
   constructor(private http: HttpClient) { }
 
@@ -15,6 +16,10 @@ export class ClienteService {
     return this.http.get(this.urlEndPoint).pipe(
       map(response => response as Cliente[])
     );
+  }
+
+  create(cliente: Cliente): Observable<Cliente>{
+    return this.http.post<Cliente>(this.urlEndPoint,cliente,{headers:this.httpHeaders})
   }
 
 
